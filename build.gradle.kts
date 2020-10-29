ef83f9179c0bd894dc73b0c9f54aa7ffc3961c5c
Yamada Koji <kou64yama@gmail.com>
2020-10-29 22:48:20 +0900

    :wrench: Add settings for ktlint (#9)

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     kotlin("plugin.spring") version "1.4.10"
     id("org.springframework.boot") version "2.3.4.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
+    id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
 dependencyLocking {
